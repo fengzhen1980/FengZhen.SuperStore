efb6931c4c4a47d7ceecbdb0939403167770e8e2
fengzhen1980 <tj.fengzhen@hotmail.com>
2020-02-26 14:41:17 -0500

    2020-02-26 02

---
+++ FengZhen.SuperStore/FengZhen.SuperStore.UI.SuperStoreForm/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FengZhen.SuperStore.UI.SuperStoreForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AdminButton = new System.Windows.Forms.Button();
+            this.customer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AdminButton
+            // 
+            this.AdminButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AdminButton.Location = new System.Drawing.Point(99, 153);
+            this.AdminButton.Name = "AdminButton";
+            this.AdminButton.Size = new System.Drawing.Size(179, 97);
+            this.AdminButton.TabIndex = 0;
+            this.AdminButton.Text = "Administrator";
+            this.AdminButton.UseVisualStyleBackColor = true;
+            this.AdminButton.Click += new System.EventHandler(this.AdminButton_Click);
+            // 
+            // customer
+            // 
+            this.customer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customer.Location = new System.Drawing.Point(481, 153);
+            this.customer.Name = "customer";
+            this.customer.Size = new System.Drawing.Size(179, 97);
+            this.customer.TabIndex = 1;
+            this.customer.Text = "Customer";
+            this.customer.UseVisualStyleBackColor = true;
+            this.customer.Click += new System.EventHandler(this.customer_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customer);
+            this.Controls.Add(this.AdminButton);
+            this.Name = "MainForm";
+            this.Text = "Super Store";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AdminButton;
+        private System.Windows.Forms.Button customer;
+    }
+}
+
---
+++ FengZhen.SuperStore/FengZhen.SuperStore.UI.SuperStoreForm/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FengZhen.SuperStore.UI.SuperStoreForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminButton_Click(object sender, EventArgs e)
+        {
+            AdminForm adminForm = new AdminForm();
+            adminForm.Show();
+        }
+
+        private void customer_Click(object sender, EventArgs e)
+        {
+            CustomerForm customerForm = new CustomerForm();
+            customerForm.Show();
+        }
+    }
+}
